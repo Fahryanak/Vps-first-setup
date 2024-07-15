4b695fc42bb37e8e5fe550e59fad76fba2577143
Fahryanak <160597974+Fahryanak@users.noreply.github.com>
2024-07-15 16:30:09 +0700

    Create systemctl.sh

---
+++ systemctl.sh
@@ -0,0 +1,3 @@
+apt install -y curl wget git python3
+curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+chmod -R 777 /bin/systemctl
